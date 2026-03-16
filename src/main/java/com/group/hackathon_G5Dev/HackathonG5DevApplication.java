15b4bbcea86ca84c6bba44a7ee50692502e7a31b
Pescot-Jourdan Florian <florianpescot4@gmail.com>
2026-03-16 11:15:33 +0100

    Initial Commit

---
+++ src/main/java/com/group/hackathon_G5Dev/HackathonG5DevApplication.java
@@ -0,0 +1,13 @@
+package com.group.hackathon_G5Dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonG5DevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonG5DevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group/hackathon_G5Dev/HackathonG5DevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.hackathon_G5Dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonG5DevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
